31aa2838da307c0a16bc5a09c93eea54d56b4ebd
unknown <you@example.com>
2024-06-09 13:00:32 +0100

    lubababa

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
 print ("hello world")
 print ("we are learning git")
- 
+
+def fahrenheit_to_celsius(fahrenheit):
+     celsius = (fahrenheit - 32) * 5/9
+     return celsius
+
+def main():
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit} degrees Fahrenheit is equal to {celsius:.2f} degrees Celsius.")
+
+if __name__ == "__main__":
+    main()
